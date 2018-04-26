e142a8e0e5615c1485ec946f00feb82951f9673f
allenlinli <allenlinli@gmail.com>
2018-04-26 15:43:12 +0800

    feat: add ECCameraPreviewView

---
+++ ErizoClient/ECCameraPreviewView.h
@@ -0,0 +1,12 @@
+//
+//  ECCameraPreviewView.h
+//  ErizoClient
+//
+//  Created by Allen and Kim on 2018/4/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECCameraPreviewView : UIView
+
+@end
---
+++ ErizoClient/ECCameraPreviewView.m
@@ -0,0 +1,21 @@
+//
+//  ECCameraPreviewView.m
+//  ErizoClient
+//
+//  Created by Allen and Kim on 2018/4/26.
+//
+
+@import WebRTC;
+#import "ECCameraPreviewView.h"
+
+@implementation ECCameraPreviewView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
